08c47691c96f8e0c2ee91c1f8761cf2b20114d52
Subarna Khadka <90277365+SubarnaKhadka@users.noreply.github.com>
2021-09-12 00:16:27 -0700

    update main.c

--- main.c
+++ main.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<windows.h>
-#include<stdbool.h>
-// #include "AddNewRecord.c"
-// #include "ATMService.c"
-// #include "DeleteRecord.c"
-// #include "FileRecord.c"
-// #include "Loan.c"
-// #include "LoginSystem.c"
-// #include "MoneyDeposit.c"
-// #include "MoneyTransaction.c"
-// #include "Options.c"
-// #include "UpdateRecord.c"
-
-
+#include "AddNewRecord.c"
+#include "ATMService.c"
+#include "DeleteRecord.c"
+#include "FileRecord.c"
+#include "Loan.c"
+#include "LoginSystem.c"
+#include "MoneyDeposit.c"
+#include "MoneyTransaction.c"
+#include "Options.c"
+#include "UpdateRecord.c"
 
 int main(){
- 
+ //code initialized
 }
