212be1d10522c6ef9c94eb06658943b6bb9ad07e
thenewbie0000 <shrestharojesh34@gmail.com>
2021-09-16 18:17:21 +0545

    Code Initiated

--- DeleteRecord.c
+++ DeleteRecord.c
@@ -1,3 +1,26 @@
-void erase(){
-	printf("Delete Account");
+#include "AddNewRecord.c"
+
+int main(){
+    FILE *ptr, *del;
+	int num, found;
+    ptr= fopen("C:\\\\newfile.txt","r+b");
+	del = fopen("C:\\\\temporary.txt", "ab");
+	printf("Enter account number of bank account which you want to close:\n");
+	scanf("%d", &num);
+	rewind(ptr);
+	found =0;
+	while ((fread(&add, sizeof(add), 1, ptr)) == 1){
+		if (num == add.account_no)
+		found++;
+		else
+		fwrite(&add, sizeof(add), 1, del);
+	}
+    if (found == 0)
+    printf("Record Not Found!!\n"); 
+    fclose(ptr);
+    fclose(del);
+    remove("C:\\\\newfile.txt");   
+    rename("C:\\\\temporary.txt", "C:\\\\newfile.txt");   
+    getch();
+    printf("Your Bank Account and all the transaction related to it is deleted.\n");
 }
